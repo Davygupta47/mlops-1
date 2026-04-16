d7c90b4b872618b8cfb44b31a5bb75fd42117807
Davygupta47 <dwaipayan.dg07@gmail.com>
2026-04-16 19:09:05 +0530

    data ingestion performed

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -2,10 +2,33 @@ import polars as pl
 import os
 
 def preprocess():
-    df = pl.read_csv("data/raw/data.csv")
-    df["rating"] = df["rating"].apply(lambda x:1 if x >= 4 else 0)
-    os.makedir("data/processed", exist_ok=True)
-    df.write_csv("data/processed/train.csv", index=False)
-    print("Preprocessing done!!")
+    ratings = pl.read_csv("data/raw/ml-25m/ratings.csv")
+    movies = pl.read_csv("data/raw/ml-25m/movies.csv")
 
-if __name__ == "__main__":    preprocess()
+    ratings = ratings.with_columns(
+        pl.from_epoch("timestamp").alias("datetime")
+    )
+#Implicit feedback (like/dislike)
+    ratings = ratings.with_columns(
+        (pl.col("rating") >= 3.5).cast(pl.Int8).alias("liked")
+    )
+#Filtering active users
+    user_counts = ratings.group_by("userId").count()
+    active_users = user_counts.filter(pl.col("count") > 50)["userId"]
+
+    ratings = ratings.filter(pl.col("userId").is_in(active_users))
+    df = ratings.join(movies, on="movieId")
+#Extraction of genres into list
+    df = df.with_columns(
+        pl.col("genres").str.split("|")
+    )
+
+    os.makedirs("data/processed", exist_ok=True)
+    df.write_parquet("data/processed/data.parquet")
+
+    print("Preprocessing complete")
+    print(df.head())
+
+
+if __name__ == "__main__":
+    preprocess()
