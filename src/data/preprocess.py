8da991c0c606383fe3ae949cc5d6b0addbc3c8bb
Davygupta47 <dwaipayan.dg07@gmail.com>
2026-04-16 19:34:23 +0530

    update the is_in usage, added build features function with utils files

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -16,7 +16,7 @@ def preprocess():
     user_counts = ratings.group_by("userId").count()
     active_users = user_counts.filter(pl.col("count") > 50)["userId"]
 
-    ratings = ratings.filter(pl.col("userId").is_in(active_users))
+    ratings = ratings.filter(pl.col("userId").is_in(active_users.to_list()))   
     df = ratings.join(movies, on="movieId")
 #Extraction of genres into list
     df = df.with_columns(
